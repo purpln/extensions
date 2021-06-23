89b6a136fd85cc274102fd7e0d1803de5ff4a9b4
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-06-23 15:31:30 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -7,5 +7,5 @@ let package = Package(
     platforms: [ .iOS(.v10), .tvOS(.v9), .watchOS(.v2), .macOS(.v10_10) ],
     products: [ .library(name: "extensions", targets: ["extensions"]) ],
     dependencies: [],
-    targets: [ .target(name: "extensions", dependencies: []) ]
+    targets: [ .target(name: "extensions", dependencies: [], path: "Sources") ]
 )
